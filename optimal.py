5f1d769d0cf57ce143ad73b290289a63ad043cbc
Ekanshi Agrawal <ekanshi258@gmail.com>
2019-10-30 12:46:08 +0530

    Optimal modelling

---
+++ optimal.py
@@ -0,0 +1,27 @@
+from nlp import dataf
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+
+#baseline
+print(dataf['subreddit'].value_counts(normalize = True))
+
+#Predictor (x) and target (y)
+x = dataf['title']
+y = dataf['subreddit']
+xtrain, xtest, ytrain, ytest = train_test_split(x,y, random_state=42, stratify = y)
+
+#GridSearchCV
+pipe = Pipeline([('vect', CountVectorizer()), ('nb', MultinomialNB())])
+params = {'vect__ngram_range':[(1,1),(1,3)], 'nb__alpha':[0.36, 0.6]}
+
+gs = GridSearchCV(pipe, param_grid = params, cv = 3)
+gs.fit(xtrain,ytrain)
+
+print("best score: ",gs.best_score_)
+print("train score: ", gs.score(xtrain,ytrain))
+print("test score: ",gs.score(xtest,ytest))
+
+print(gs.best_params_)

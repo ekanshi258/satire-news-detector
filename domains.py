02a4184b4942900dac7ac183e003818955a34ef5
Kushagra <kushagra99@gmail.com>
2019-11-01 23:49:31 +0530

    Modified domain plots

--- domains.py
+++ domains.py
@@ -1,19 +1,19 @@
 from imports import bars
 import pandas as pd
 
-fake = pd.read_csv('fake.csv')
-real = pd.read_csv('real.csv')
+fake = pd.read_csv('./clean_data/fake.csv')
+real = pd.read_csv('./clean_data/real.csv')
 
 #Plot the fake domains vs posts
 fake_domain = fake['domain'].value_counts()
-fake_domain = fake_domain.sort_values(ascending = False).head(20)			#number of posts per domain
+fake_domain = fake_domain.sort_values(ascending = False).head(10)			#number of posts per domain
 fake_domain_idx = list(fake_domain.index)							#y-axis: domains
 
-bars(fake_domain.values, fake_domain_idx, 'Domains referenced in r/TheOnion','g')
+bars(fake_domain.values, fake_domain_idx, 'Domains referenced in fake news dataset','g')
 
 #plot notthefake domains vs posts
 real_domain = real['domain'].value_counts()
-real_domain = real_domain.sort_values(ascending=False).head(40)
+real_domain = real_domain.sort_values(ascending=False).head(10)
 real_domain_idx = list(real_domain.index)
 
-bars(real_domain.values, real_domain_idx, 'Domains referenced in r/NotTheOnion','b')
+bars(real_domain.values, real_domain_idx, 'Domains referenced in real news dataset','b')
